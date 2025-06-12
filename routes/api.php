cd2e703072af1d0efa5c40f51bdae46410ab05e2
Andrii Kravets <hyandi.work@gmail.com>
2025-06-12 11:24:41 +0300

    Add xss

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\MovieController;
 use App\Http\Middleware\CookieMiddleware;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(CookieMiddleware::class)->group(function () {
+Route::middleware([CookieMiddleware::class, \ProtoneMedia\LaravelXssProtection\Middleware\XssCleanInput::class])->group(function () {
     Route::apiResource('genre', GenreController::class);
     Route::apiResource('movie', MovieController::class);
     Route::put('/movie/{movie}/active', [MovieController::class, 'active']);
